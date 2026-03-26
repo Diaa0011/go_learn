e6bf57b21af83d2a792c2a635d100e4d89781dc0
diaadawood <diaadawood@localhost.localdomain>
2026-03-26 17:19:47 +0000

    first snippets

---
+++ hello-go-project/hello_name_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestHelloName(t *testing.T) {
+	got := HelloName("Chris")
+	want := "Hello, Chris"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+}
