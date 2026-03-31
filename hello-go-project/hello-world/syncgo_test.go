a1f4e2a5c54dfbf34489737fc6a3fee6f8391772
diaadawood <diaadawood@localhost.localdomain>
2026-03-31 14:40:05 +0200

    adding first version of propertybaedtest

--- hello-go-project/hello-world/syncgo_test.go
+++ hello-go-project/hello-world/syncgo_test.go
@@ -1,58 +1,58 @@
 package main
 
-import (
-	"sync"
-	"testing"
-)
+// import (
+// 	"sync"
+// 	"testing"
+// )
 
-func TestCounter(t *testing.T) {
-	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		counter := Counter{}
-		counter.Inc()
-		counter.Inc()
-		counter.Inc()
+// func TestCounter(t *testing.T) {
+// 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+// 		counter := Counter{}
+// 		counter.Inc()
+// 		counter.Inc()
+// 		counter.Inc()
 
-		if counter.Value() != 3 {
-			t.Errorf("got %d, want %d", counter.Value(), 3)
-		}
-	})
+// 		if counter.Value() != 3 {
+// 			t.Errorf("got %d, want %d", counter.Value(), 3)
+// 		}
+// 	})
 
-	t.Run("it runs safely concurrently", func(t *testing.T) {
-		wantedCount := 1000
-		counter := Counter{}
+// 	t.Run("it runs safely concurrently", func(t *testing.T) {
+// 		wantedCount := 1000
+// 		counter := Counter{}
 
-		var wg sync.WaitGroup
-		wg.Add(wantedCount)
+// 		var wg sync.WaitGroup
+// 		wg.Add(wantedCount)
 
-		for i := 0; i < wantedCount; i++ {
-			go func() {
-				counter.Inc()
-				wg.Done()
-			}()
-		}
+// 		for i := 0; i < wantedCount; i++ {
+// 			go func() {
+// 				counter.Inc()
+// 				wg.Done()
+// 			}()
+// 		}
 
-		wg.Wait()
-		assertCounter(t, &counter, wantedCount)
-	})
-}
+// 		wg.Wait()
+// 		assertCounter(t, &counter, wantedCount)
+// 	})
+// }
 
-func assertCounter(t testing.TB, got *Counter, want int) {
-	t.Helper()
-	if got.value != want {
-		t.Errorf("got %d, want %d", got.Value(), want)
-	}
-}
+// func assertCounter(t testing.TB, got *Counter, want int) {
+// 	t.Helper()
+// 	if got.value != want {
+// 		t.Errorf("got %d, want %d", got.Value(), want)
+// 	}
+// }
 
-func (c *Counter) Inc() {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.value++
-}
-func (c *Counter) Value() int {
-	return c.value
-}
+// func (c *Counter) Inc() {
+// 	c.mu.Lock()
+// 	defer c.mu.Unlock()
+// 	c.value++
+// }
+// func (c *Counter) Value() int {
+// 	return c.value
+// }
 
-type Counter struct {
-	mu    sync.Mutex
-	value int
-}
+// type Counter struct {
+// 	mu    sync.Mutex
+// 	value int
+// }
